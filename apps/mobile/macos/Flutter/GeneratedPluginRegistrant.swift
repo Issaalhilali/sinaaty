a2e0b28d81050b84e775dbf62a5277444211457a
issa <issa@MacBook-Pro-issa.local>
2026-09-25 10:38:23 +0300

    chore(mobile): تسجيل إضافة printing في المنصّات المولَّدة (linux/macos/windows)

--- apps/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import flutter_secure_storage_macos
 import geolocator_apple
 import mobile_scanner
 import package_info_plus
+import printing
 import record_macos
 import shared_preferences_foundation
 import speech_to_text
@@ -27,6 +28,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
